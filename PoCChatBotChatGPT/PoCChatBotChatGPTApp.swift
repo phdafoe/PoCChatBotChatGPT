a88d4231161a0b018a0cb6c5d2a01ebba58b2202
Phdafoe <phdafoe@gmail.com>
2023-04-24 12:01:32 +0200

    Initial Commit

---
+++ PoCChatBotChatGPT/PoCChatBotChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoCChatBotChatGPTApp.swift
+//  PoCChatBotChatGPT
+//
+//  Created by TECDATA ENGINEERING on 24/4/23.
+//
+
+import SwiftUI
+
+@main
+struct PoCChatBotChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
